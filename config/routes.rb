06f22d3ec8d74855e42665567fc037fe8895c65c
ayakotouma <lingzi2505@gmail.com>
2025-01-20 17:02:24 +0900

    トップページ表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # ルートパスを設定
+  root to: 'items#index'
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # 他のルーティングも必要に応じて追加可能
 end
