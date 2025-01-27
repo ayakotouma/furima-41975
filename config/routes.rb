1cf0442a015a50ff0763936d11ea6a032d21458e
ayakotouma <lingzi2505@gmail.com>
2025-01-27 12:32:35 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # ルートパスを設定
   root to: 'items#index'
   
-  resources :items, only: [:index, :show]
-  
+  resources :items, only: [:index, :show, :new, :create]
+
   # 他のルーティングも必要に応じて追加可能
 end
