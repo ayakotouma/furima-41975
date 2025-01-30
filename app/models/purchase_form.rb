71a77bb832a6440463c6455446811e1d7d55ca9b
ayakotouma <lingzi2505@gmail.com>
2025-01-30 21:16:07 +0900

    エラーの表示

--- app/models/purchase_form.rb
+++ app/models/purchase_form.rb
@@ -4,11 +4,13 @@ class PurchaseForm
   attr_accessor :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
-    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Enter it as 123-4567' }
-    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :postal_code, presence: { message: "can't be blank" },
+                            format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Enter it as 123-4567' }
+    validates :prefecture_id, presence: { message: "can't be blank" }, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
-    validates :address
-    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid. Enter it without hyphens' }
+    validates :address, presence: { message: "can't be blank" }
+    validates :phone_number, presence: { message: "can't be blank" },
+                             format: { with: /\A\d{10,11}\z/, message: 'is invalid. Enter it without hyphens' }
     validates :user_id
     validates :item_id
     # validates :token
